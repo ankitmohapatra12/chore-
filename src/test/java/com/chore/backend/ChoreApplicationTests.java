ef8d4958f9f55d2562d2512606f96dab373ac0f4
Ankit Mohapatra <ankitmohapatra033@gmail.com>
2024-03-11 01:10:31 +0530

    added chore

---
+++ src/test/java/com/chore/backend/ChoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chore.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
